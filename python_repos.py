46bfd994ade83a31c12b0eba457bfc42fa1eff85
Dreamy <dreamy26@icloud.com>
2022-12-27 18:33:43 -0500

    chore: Adds correction to Github url

--- python_repos.py
+++ python_repos.py
@@ -1,12 +1,12 @@
 import requests 
  # Make an API call and store the response
  
-url = 'https://apigithub.com/search/repositories?q=language:python&sort=stars'
-headers = {'Accept': 'application/vnd.github.v3+json'}
-r = requests.get(url, headers=headers)
-print(f"Status code: {r.status_code}")
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars' # url of the API call
+headers = {'Accept': 'application/vnd.github.v3+json'} # the version of the API
+r = requests.get(url, headers=headers) # request to make the call to API
+print(f"Status code: {r.status_code}") # print status_code, to make sure the call went through
 
-# Store API response ina variable.
+# Store API response in a variable.
 response_dict = r.json()
 
 # Process results.
