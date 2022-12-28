b68f44b1b671db80289d73c4f96210dc5b8183ec
Dreamy <dreamy26@icloud.com>
2022-12-27 20:23:10 -0500

    build: Adds for loop to print the GH dictionaries

--- python_repos.py
+++ python_repos.py
@@ -28,10 +28,11 @@ repo_dict = repo_dicts[0]
     
 # PULL some values from the keys
 print("\nSelected information about first repository:")
-print(f"Name: {repo_dict['name']}") # Owner of repo Name
-print(f"Owner: {repo_dict['owner']['login']}") # Owner login Name
-print(f"Stars: {repo_dict['stargazers_count']}") # Stars the project has earned
-print(f"Repository: {repo_dict['html_url']}") # URL for project
-print(f"Created: {repo_dict['created_at']}") # When repo was Created
-print(f"Updated: {repo_dict['updated_at']}") # When repo was last Updated
-print(f"Description: {repo_dict['description']}") # Repository printed Description
+for repo_dict in repo_dicts: # Loop, to iterate through all the dictionaries
+    print(f"Name: {repo_dict['name']}") # Owner of repo Name
+    print(f"Owner: {repo_dict['owner']['login']}") # Owner login Name
+    print(f"Stars: {repo_dict['stargazers_count']}") # Stars the project has earned
+    print(f"Repository: {repo_dict['html_url']}") # URL for project
+    print(f"Created: {repo_dict['created_at']}") # When repo was Created
+    print(f"Updated: {repo_dict['updated_at']}") # When repo was last Updated
+    print(f"Description: {repo_dict['description']}") # Repository printed Description
