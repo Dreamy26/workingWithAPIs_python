919fe640c9fdd1c800c99ee2581efcb43c05d4ec
Dreamy <dreamy26@icloud.com>
2022-12-27 20:37:55 -0500

    build: Adds link to see GitHubs rate limits

--- python_repos.py
+++ python_repos.py
@@ -27,12 +27,14 @@ repo_dict = repo_dicts[0]
 #     print(key)
     
 # PULL some values from the keys
-print("\nSelected information about first repository:")
+print("\nSelected information about each repository:")
 for repo_dict in repo_dicts: # Loop, to iterate through all the dictionaries
-    print(f"Name: {repo_dict['name']}") # Owner of repo Name
+    print(f"\nName: {repo_dict['name']}") # Owner of repo Name
     print(f"Owner: {repo_dict['owner']['login']}") # Owner login Name
     print(f"Stars: {repo_dict['stargazers_count']}") # Stars the project has earned
     print(f"Repository: {repo_dict['html_url']}") # URL for project
     print(f"Created: {repo_dict['created_at']}") # When repo was Created
     print(f"Updated: {repo_dict['updated_at']}") # When repo was last Updated
     print(f"Description: {repo_dict['description']}") # Repository printed Description
+    # In order to see GH API rate limits 
+    # https://api.github.com/rate_limit
